f3cca24b82c2b68767eb8091d8d474ad545f5e9a
xavii07 <xaviergaspa123@gmail.com>
2024-01-03 20:15:26 -0500

    settings.py

--- navidad/settings.py
+++ navidad/settings.py
@@ -22,7 +22,7 @@ import os
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.enviroment.get('SECRET_KEY', default='sdfds66fs6d6f6ds6f6')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='sdfds66fs6d6f6ds6f6')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
